e2bbac6bd1dfe5844b2f30a4e86d6820993e6181
raghavsinghgoud <raghavsinghgoud@gmail.com>
2026-05-27 18:04:21 +0530

    dictionary

---
+++ PythonPractice/dictionaries.py
@@ -0,0 +1,89 @@
+# dictionary = {
+#     "cat": "chat",
+#     "dog": "chien",
+#     "horse": "cheval"
+# }
+# phone_numbers= {'boss': 454545454545, 'suzy':37373737337}
+# empty_dictionary = {}
+
+# print(dictionary)
+# print(type(dictionary))
+# print(phone_numbers)
+# print(type(phone_numbers))
+# print(empty_dictionary)
+# print(type(empty_dictionary))
+
+
+'''calling elements'''
+
+# print(dictionary['cat'])
+# print(phone_numbers['suzy'])
+
+
+#print(phone_numbers['President']) '''type error'''
+
+
+# Words = {'cat', 'lion', 'horse'}
+# for word in Words:
+#     if word in dictionary:
+#         print(word, "->", dictionary[word])
+#     else:
+#         print(word, "is not in dictionary")
+
+'''items()'''
+
+# print(dictionary.keys())
+# for key in dictionary.keys():
+#     print(key,"->", dictionary[key])
+
+
+# for key,value in dictionary.items():
+#     print(key,"->", value)
+
+'''values()'''
+
+# print(dictionary.values())
+
+# for value in dictionary.values():
+#     print(value)
+
+'''copy()'''
+
+# pol_eng_dictionary = {
+#     "zamek": "castle",
+#     "woda": "water",
+#     "gleba": "soil"
+# }
+# print("pol_eng_dictionary: ", pol_eng_dictionary)
+# copy_dictionary = pol_eng_dictionary.copy()
+
+# print("copy_dictionary: ", copy_dictionary)
+
+# pol_eng_dictionary["zamek"]= "lock" # assigning new value
+# item = pol_eng_dictionary["zamek"]
+# print(item)
+
+# phonebook ={}
+# print(phonebook)
+# phonebook["Adam"]= 34563524623 # create or add a key-value pair
+# print(phonebook)
+
+# del phonebook["Adam"]
+# print(phonebook)
+
+
+'''update(), popitem()'''
+
+# pol_eng_dictionary = {"kwiat": "flower"}
+
+# pol_eng_dictionary.update(
+#     {
+#         "gleba":"soil"
+#     }
+# )
+# print(pol_eng_dictionary)
+
+# pol_eng_dictionary.popitem()
+# print(pol_eng_dictionary)
+
+
--- PythonPractice/tuples.py
+++ PythonPractice/tuples.py
@@ -1,12 +1,63 @@
 
-my_tuple = (1,10,100)
+# my_tuple = (1,10,100)
 
-t1 = my_tuple + (1000,10000)
-t2 = my_tuple *3
+# t1 = my_tuple + (1000,10000)
+# t2 = my_tuple *3
 
-print(len(t2))
-print(t1)
-print(t2)
-print(10 in my_tuple)
-print(-10 not in my_tuple)
+# print(len(t2))
+# print(t1)
+# print(t2)
+# print(10 in my_tuple)
+# print(-10 not in my_tuple)
 
+
+# my_tuple = (10,100,1000)
+# print(id(my_tuple)) #finding address
+# my_tuple += (10000,100000)
+# print(my_tuple)
+# print(id(my_tuple))
+
+
+# tuple_1 = (1,2,3)
+# for elem in tuple_1:
+#     print(elem)
+
+# tuple_2 = (1,2,3,4)
+# print(5 in tuple_2)
+# print(5 not in tuple_2)
+
+# tuple_3 = (1,2,3,4)
+# print(len(tuple_3))
+# print(5 not in tuple_3)
+
+# tuple_4 = tuple_1 + tuple_2
+# tuple_5 = tuple_3 *2
+# print(tuple_4)
+# print(tuple_5)
+
+
+'''converting list to tuple'''
+
+# my_tuple = tuple((1,2,"String"))
+# print(my_tuple)
+# print(type(my_tuple))
+
+# my_list = [2,4,6]
+# print(my_list)
+# print(type(my_list))
+# tup = tuple(my_list)
+# print(tup)
+# print(type(tup))
+
+
+'''use of tuple'''
+
+# var = 123
+
+# t1 = (1,)
+# t2 = (2,)
+# t3 = (3, var)
+
+# t1,t2,t3=t2,t3,t1
+# print(t1,t2,t3)
+# print(type(t1),type(t2),type(t3))
