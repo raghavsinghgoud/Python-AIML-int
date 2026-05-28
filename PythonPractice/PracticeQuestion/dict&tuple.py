7c5a3a310e5609ab4c4a9924af6c40b66fc7bc74
raghavsinghgoud <raghavsinghgoud@gmail.com>
2026-05-28 17:52:31 +0530

    commit

--- PythonPractice/PracticeQuestion/dict&tuple.py
+++ PythonPractice/PracticeQuestion/dict&tuple.py
@@ -5,8 +5,8 @@
 #     if name == "":
 #         break
 
-#     score = int(input("Enter the student's score (0 - 10):"))
-#     if score not in range(0, 11):
+#     score = int(input(f"Enter ${name}'s score (0 - 10):")) # f-string used
+#     if score not in range(1, 11):
 #         break
 
 #     if name in school_class:
@@ -14,11 +14,22 @@
 #     else:
 #         school_class[name] = (score, )
 
-# for name in sorted(school_class):
+
+# for name , mark in school_class.items():
+#     sum = 0
+#     for m in mark:
+#         # print(m)
+#         sum += m
+#     print(name, "->", sum/len(mark))
+# print(school_class)
+
+
+# for name in sorted(school_class.keys()):
 #     marks = 0
 #     number = 0
 #     for score in school_class[name]:
 #         marks += score
 #         number += 1
 #     print(name, ":", marks/number)
+
 
