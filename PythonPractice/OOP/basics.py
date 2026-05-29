ee29ce25d25eda618341891d3b5c04d86d130c93
raghavsinghgoud <raghavsinghgoud@gmail.com>
2026-05-29 18:01:12 +0530

    OOPs

---
+++ PythonPractice/OOP/basics.py
@@ -0,0 +1,57 @@
+# class ThisIsMyFirstClass:
+#     name = "Raghav" #property or attributes
+#     age = 19
+    
+#     def getName(self): #method
+#         print(self.name)
+
+# firstObject = ThisIsMyFirstClass()
+# print(firstObject)
+
+# firstObject.getName() #calling method
+# print(firstObject.name)
+
+
+
+# class Student :
+#     def __init__(self):  # __init__ is a contructor
+#         self.name = ""
+#         self.age = 0
+#         self.gender = ""
+#         self.grade = ""
+
+# raghav = Student()
+# print(raghav)
+
+# raghav.name = "Raghav Singh Goud"
+# raghav.age = 19
+# raghav.gender = "Male"
+# raghav.grade = "10th"
+
+# print(raghav.name)
+# print(raghav.age)
+# print(raghav.gender)
+# print(raghav.grade)
+
+# print(raghav.__dict__) #creates a dictionary of object
+
+
+
+# class Student :
+#     def __init__(self, name, age, gender, grade):  # __init__ is a contructor
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+#         self.grade = grade
+
+#     def printDetails(self):
+#         print("Name:",self.name)
+#         print("Age:",self.age)
+#         print("Gender:",self.gender)
+#         print("Grade:",self.grade)
+
+# raghav = Student("Raghav Singh Goud", 19, "Male", "10th")
+# print(raghav)
+
+# raghav.printDetails()
+
