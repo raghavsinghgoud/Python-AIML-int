e6fbaa90ed5996ae2a775e00aba0c650783626be
raghavsinghgoud <raghavsinghgoud@gmail.com>
2026-06-19 01:33:19 +0530

    Assignment Question

--- AssignmentQuestions/Libraries/q1.py
+++ AssignmentQuestions/Libraries/q1.py
@@ -1,2 +1,26 @@
 '''Ask user for: first name, last name, age, city, favourite skill. Print a formatted name card with padding alignment'''
 
+import pandas as pd
+
+# first_name= input("Enter Your First Name: ")
+# last_name= input("Enter Your Last Name: ")
+# age= input('Enter your Age: ')
+# city= input('Enter your City: ')
+# skill = input('Enter your Favourite Skills: ')
+
+# name = f'{first_name} {last_name}'
+# age = f'{age} years'
+# data = {
+#     'Field':['Name','Age','City','Skill'],
+#     'Value':[name, age, city, skill]
+# }
+
+# df = pd.DataFrame(data)
+
+# width = 60
+# print('+'+'-'*width +'+')
+
+# for _, row in df.iterrows():
+#     print(f'| {row['Field']:<8}: {row['Value']:<49}|')
+
+# print('+'+'-'*width +'+')
