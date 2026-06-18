22280a7898bc418f6d1a54c0b73b1123e84e59db
raghavsinghgoud <raghavsinghgoud@gmail.com>
2026-06-18 17:42:38 +0530

    Probability

--- Machine Learning/Probability/basics.py
+++ Machine Learning/Probability/basics.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm                # Normal Distribution Calculator
 
-#You feed it a mean and standard deviation , and it can answer any probability question about q.
+# #You feed it a mean and standard deviation , and it can answer any probability question about q.
 
-#Normal Distribution - The bell curve 
-#Normal Distribution with mean 165cm and standard deviation 7cm.
-mean_h, std_h = 165,7
+# #Normal Distribution - The bell curve 
+# #Normal Distribution with mean 165cm and standard deviation 7cm.
+# mean_h, std_h = 165,7
 
-# Probability of being taller than 175cm
-prob = 1 - norm.cdf(175, mean_h, std_h) # Cumulative distribution function, tells how many prob.
-print(f'P(height > 175cm) = {prob:.4f} = {prob>100:.1f}%')
+# # Probability of being taller than 175cm
+# prob = 1 - norm.cdf(175, mean_h, std_h) # Cumulative distribution function, tells how many prob.
+# print(f'P(height > 175cm) = {prob:.4f} = {prob>100:.1f}%')
 
-# The 68-95-99.7 Rule
-print(f'68% of people: {mean_h-std_h:.0f}cm to {mean_h+std_h:.0f}cm')
-print(f'95% of people: {mean_h-2*std_h:.0f}cm to {mean_h+2*std_h:.0f}cm')
-print(f'99% of people: {mean_h-3*std_h:.0f}cm to {mean_h+3*std_h:.0f}cm')
+# # The 68-95-99.7 Rule
+# print(f'68% of people: {mean_h-std_h:.0f}cm to {mean_h+std_h:.0f}cm')
+# print(f'95% of people: {mean_h-2*std_h:.0f}cm to {mean_h+2*std_h:.0f}cm')
+# print(f'99% of people: {mean_h-3*std_h:.0f}cm to {mean_h+3*std_h:.0f}cm')
