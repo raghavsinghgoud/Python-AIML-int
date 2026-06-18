7f601d6d03dba887ee8ebadc0dc9e570adb073be
raghavsinghgoud <raghavsinghgoud@gmail.com>
2026-06-18 17:41:14 +0530

    Correlation

--- Machine Learning/Probability/basics.py
+++ Machine Learning/Probability/basics.py
@@ -1,3 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy.stats import norm                # Normal Distribution Calculator
+
+#You feed it a mean and standard deviation , and it can answer any  probability question about 
+
+#Normal Distribution - The bell curve 
+#Normal Distribution with mean 165cm and standard deviation 7cm.
+mean_h, std_h = 165,7
+
+# Probability of being taller than 175cm
+prob = 1 - norm.cdf(175, mean_h, std_h) # Cumulative distribution function, tells how many 
+print(f'P(height > 175cm) = {prob:.4f} = {prob>100:.1f}%')
+
+# The 68-95-99.7 Rule
+print(f'68% of people: {mean_h-std_h:.0f}cm to {mean_h+std_h:.0f}cm')
+print(f'95% of people: {mean_h-2*std_h:.0f}cm to {mean_h+2*std_h:.0f}cm')
+print(f'99% of people: {mean_h-3*std_h:.0f}cm to {mean_h+3*std_h:.0f}cm')
